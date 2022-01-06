1b0f5de1a9ee79e8048e448c1c6287751468c359
Natalia Souza <nataliasouza.ti@hotmail.com>
2022-01-05 23:48:39 -0300

    Interfaces

---
+++ Feature/Clients/Interfaces/IClientRepository.cs
@@ -0,0 +1,9 @@
+﻿using Feature.Core;
+
+namespace Feature.Clients
+{
+    public interface IClientRepository : IRepository<Client>
+    {
+        Client GetByEmail(string email);
+    }
+}
---
+++ Feature/Clients/Interfaces/IClientService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Feature.Clients
+{
+    public interface IClientService
+    {
+        public IEnumerable<Client> GetAllActive();
+        public void AddClient(Client client);
+    }
+}
