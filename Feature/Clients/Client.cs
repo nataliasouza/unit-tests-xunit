2b3c0bbcfdd42c58132f4d687fda3e01fcb62d8b
Natalia Souza <nataliasouza.ti@hotmail.com>
2022-01-05 22:25:53 -0300

    I created the class Client, ClientValidation and Entity

---
+++ Feature/Clients/Client.cs
@@ -0,0 +1,51 @@
+﻿using Feature.Core;
+using Feature.Core.Validations;
+using System;
+
+namespace Feature.Clients
+{
+    public class Client : Entity
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthday { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public string Email { get; set; }
+        public bool Active { get; set; }
+
+        protected Client() { }
+
+        public Client(Guid id, string name, DateTime birthday,
+            string email, bool active, DateTime registrationDate)
+        {
+            Id = id;
+            Name = name;
+            Surname = Surname;
+            Birthday = birthday;
+            RegistrationDate = registrationDate;
+            Email = email;
+            Active = active;
+        }
+
+        public string FullName()
+        {
+            return $"{Name} { Surname}";
+        }
+
+        public bool IsEspecial()
+        {
+            return RegistrationDate < DateTime.Now.AddYears(-3) && Active;
+        }
+
+        public void Inactivate()
+        {
+            Active = false;
+        }
+
+        public override bool IsValid()
+        {
+            ValidationResult = new ClientValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+    }
+}
---
+++ Feature/Core/Validations/ClientValidation.cs
@@ -0,0 +1,38 @@
+﻿using Feature.Clients;
+using System;
+using FluentValidation;
+
+namespace Feature.Core.Validations
+{
+    public class ClientValidation : AbstractValidator<Client>
+    {
+
+        public ClientValidation()
+        {
+            RuleFor(c => c.Name)
+                .NotEmpty().WithMessage("Por favor, certifique-se de ter inserido o nome")
+                .Length(2, 150).WithMessage("O nome deve ter entre 2 e 150 caracteres");
+
+            RuleFor(c => c.Surname)
+                .NotEmpty().WithMessage("Por favor, certifique-se de ter inserido o sobrenome")
+                .Length(2, 150).WithMessage("O Sobrenome deve ter entre 2 e 150 caracteres");
+
+            RuleFor(c => c.Birthday)
+                .NotEmpty()
+                .Must(HaveMinimumAge)
+                .WithMessage("O cliente deve ter 18 anos ou mais");
+
+            RuleFor(c => c.Email)
+                .NotEmpty()
+                .EmailAddress();
+
+            RuleFor(c => c.Id)
+                .NotEqual(Guid.Empty);
+        }
+
+        public static bool HaveMinimumAge(DateTime birthDate)
+        {
+            return birthDate <= DateTime.Now.AddYears(-18);
+        }
+    }
+}
