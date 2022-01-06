f872a2e917629a243448fc4166270153c4c34337
Natalia Souza <nataliasouza.ti@hotmail.com>
2022-01-05 23:37:22 -0300

    IRepository

--- Feature/Core/IRepository.cs
+++ Feature/Core/IRepository.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq.Expressions;
 
 namespace Feature.Core
 {
-    public class IRepository
+    public interface IRepository<TEntity> : IDisposable where TEntity : Entity
     {
+        void Add(TEntity obj);
+        TEntity GetById(Guid id);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> predicate);
+        void Update(TEntity obj);
+        void Delete(Guid id);        
+        int Commit();
     }
 }
