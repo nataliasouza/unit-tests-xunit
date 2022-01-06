f30528bedb7ae71daf06559326fa32cce848bc3b
Natalia Souza <nataliasouza.ti@hotmail.com>
2022-01-05 23:02:35 -0300

    ClientEmailNotification

--- Feature/Clients/ClientEmailNotification.cs
+++ Feature/Clients/ClientEmailNotification.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
 
 namespace Feature.Clients
 {
-    public class ClientEmailNotification
-    {
+    public class ClientEmailNotification : INotification
+    {        
+        public string Origin { get; private set; }
+        public string Destiny { get; private set; }
+        public string Matters { get; private set; }
+        public string Message { get; private set; }
+
+        public ClientEmailNotification(string origin, string destiny, string matters, string message)
+        {
+            Origin = origin;
+            Destiny = destiny;
+            Matters = matters;
+            Message = message;
+        }
     }
 }
