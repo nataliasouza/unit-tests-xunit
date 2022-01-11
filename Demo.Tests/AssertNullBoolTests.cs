5d477b18ddd9b21b4c0a593740ba94b235f91121
Natalia Souza <nataliasouza.ti@hotmail.com>
2022-01-10 22:05:22 -0300

    AssertNullBollTests

---
+++ Demo.Tests/AssertNullBoolTests.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+
+namespace Demo.Tests
+{
+    public class AssertNullBoolTests
+    {
+        [Fact]
+        public void Employee_Name_NotNullOrEmpty()
+        {
+            //Arrange and Act
+            var employee = new Employee(name: "", salary: 4000);
+
+            //Assert
+            Assert.False(string.IsNullOrEmpty(employee.Name));
+        }
+        
+        [Fact]
+        public void Employee_Surname_NotHaveSurname()
+        {
+            //Arrange and Act
+            var employee = new Employee(name: "Natalia", salary: 4000);
+
+            //Assert
+            Assert.Null(employee.Name);
+
+            //Assert boll
+            Assert.True(string.IsNullOrEmpty(employee.Surname));
+            Assert.False(condition: employee.Surname?.Length > 0);
+        }
+    }
+}
