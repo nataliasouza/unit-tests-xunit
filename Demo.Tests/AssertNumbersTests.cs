51adb5b17c0077a8c3caf46442706cbe03e1f626
Natalia Souza <nataliasouza.ti@hotmail.com>
2022-01-10 21:42:57 -0300

    AssertNumbersTests

---
+++ Demo.Tests/AssertNumbersTests.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+
+namespace Demo.Tests
+{
+    public class AssertNumbersTests
+    {
+        [Fact]
+        public void Calculator_Addition_MustBeEquals()
+        {
+            //Arrange
+
+            var calculator = new Calculator();
+
+            //Act   
+            var result = calculator.Addition(3, 2);
+
+            Assert.Equal(expected: 5, actual: result);
+        }
+
+        [Fact]
+        public void Calculator_Addition_MustNotBeEquals()
+        {
+            //Arrange
+
+            var calculator = new Calculator();
+
+            //Act   
+            var result = calculator.Addition(3.131331313331, 2.87643263);
+
+            Assert.NotEqual(expected: 5.9, actual: result, precision:1);
+        }
+
+    }
+}
