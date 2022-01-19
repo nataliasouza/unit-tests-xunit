0af7dd89eb7ff9ad4ff76aeff42b8442eca0728e
Natalia Souza <nataliasouza.ti@hotmail.com>
2022-01-18 21:02:32 -0300

    ClientTests

---
+++ Features.Tests/01-Traits/ClientTests.cs
@@ -0,0 +1,51 @@
+﻿using Feature.Clients;
+using System;
+using Xunit;
+
+namespace Features.Tests._01_Traits
+{
+    public class ClientTests
+    {
+        [Fact]
+        public void Client_NewClient_MustBeValid()
+        {
+            //Arrange
+            var cliente = new Client(
+                Guid.NewGuid(),
+                name: "Natt",
+                surname: "Souza",
+                birthday: DateTime.Now.AddYears(-37),
+                email: "natt@nso.com.br",
+                active: true,
+                registrationDate: DateTime.Now);
+
+            //Act
+            var result = cliente.IsValid();
+
+            //Assert
+            Assert.True(result);
+            Assert.Equal(expected: 0, actual: cliente.ValidationResult.Errors.Count);
+        }
+
+        [Fact]
+        public void Client_NewClient_MustBeNotValid()
+        {
+            //Arrange
+            var cliente = new Client(
+                Guid.NewGuid(),
+                name: "",
+                surname: "",
+                birthday: DateTime.Now.AddYears(-37),
+                email: "natt@nso.com.br",
+                active: true,
+                registrationDate: DateTime.Now);
+
+            //Act
+            var result = cliente.IsValid();
+
+            //Assert
+            Assert.False(result);
+            Assert.NotEqual(expected: 0, actual: cliente.ValidationResult.Errors.Count);
+        }
+    }
+}
